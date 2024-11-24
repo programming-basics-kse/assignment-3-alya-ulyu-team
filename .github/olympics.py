d4ee4e3c700ef55bdd29b65fa8658256bbc87f9d
Kleimonov Bohdan <bkleimonov@kse.org.ua>
2024-11-24 21:43:19 +0200

    додав конструкцію для зчитування файлу з даними та перевірку на потрібні колонки в хедері цього ж файлу

--- .github/olympics.py
+++ .github/olympics.py
@@ -1,10 +1,19 @@
 import argparse
 import os
+from idlelib.iomenu import encoding
+
 
 def perevirka(file_path, country, year, output=None)
     if not os.path.exists(file_path):
         print(f"Well fella, файл {file_path} відсутній!")
         return
+
+    with open(file_path, 'r', encoding='utf-8') as data_mandata
+        columns = data_mandata.readline().strip().split('\t')
+
+        if "Name" not in columns or "Sport" not in columns or "Medal" not in columns or "Team" not in columns or "Year" not in columns:
+            print("Well fella, у файлі відсутні потрібні колонки!")
+            return
 
 parser = argparse.ArgumentParser(description="Обробка даних за 120 років Олімпійських ігор")
 parser.add_argument("file", help="Шлях до файлу")
