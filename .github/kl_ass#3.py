8396e75f0634b9cc857b6f907a681925f1856e8e
Kleimonov Bohdan <bkleimonov@kse.org.ua>
2024-11-24 21:09:22 +0200

    додав опис аргументів

--- .github/kl_ass#3.py
+++ .github/kl_ass#3.py
@@ -1,1 +1,10 @@
 import argparse
+import os
+
+parser = argparse.ArgumentParser(description="Обробка даних за 120 років Олімпійських ігор")
+parser.add_argument("file", help="Шлях до файлу")
+parser.add_argument("-medals", required=True, help="Назва країни? Або її код")
+parser.add_argument("year", help="Рік у якому були Олімпійські ігри")
+parser.add_argument("-output", help="опціональна функція до 1 завдання")
+
+args = parser.parse_args()
